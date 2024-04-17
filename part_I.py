283bc97479018baa3bf81f6776cda0b745dc6f59
auguzinzin <crespin.augustin@gmail.com>
2024-04-17 12:08:00 +0200

    corrections

--- part_I.py
+++ part_I.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import copy
+from proba import proba_security_dice, proba_normal_dice, proba_risky_dice
 
 nSquares = 15
 
@@ -18,205 +19,6 @@ ___________________________________________________________________
 
 """
 
-def proba_security_dice():
-    
-    proba = np.zeros((nSquares,nSquares))
-    proba_prison = np.zeros((nSquares,nSquares))
-    
-    for i in range(nSquares-1):
-        
-        #1 chance out of 2 to stay at the same place
-        proba[i][i] = 0.5
-        
-        #1 chance out of 2 to go 1 further
-        if i == 2 :
-            proba[i][i+1] = 0.25 #slow lane
-            proba[i][i+8] = 0.25 #fast lane
-            
-        elif i == 9 :
-            proba[i][i+5] = 0.5
-            
-        else :
-            proba[i][i+1] = 0.5
-    
-    proba[nSquares-1][nSquares-1] = 1 #the end
-    return proba, proba_prison
-
-def proba_normal_dice(layout, circle):
-    
-    proba = np.zeros((nSquares,nSquares))
-    proba_prison = np.zeros((nSquares,nSquares))
-    for i in range(nSquares-1):
-        
-        #1 chance out of 3 to stay at the same place
-        proba[i][i] = 1/3
-        
-        #1 chance out of 3 to go 1 further
-        #1 chance out of 3 to go 2 further
-        
-        if i == 2 :
-            proba[i][i+1] = 1/6 #slow lane
-            proba[i][i+2] = 1/6 #slow lane
-            proba[i][i+8] = 1/6 #fast lane
-            proba[i][i+9] = 1/6 #fast lane
-        
-        elif i == 8 :
-            proba[i][i+1] = 1/3
-            proba[i][i+6] = 1/3
-            
-        elif i == 9 :
-            if circle :
-                proba[i][i+5] = 1/3
-                proba[i][0] = 1/3
-            else :
-                proba[i][i+5] = 2/3
-                
-        elif i == 13 :
-            if circle :
-                proba[i][i+1] = 1/3
-                proba[i][0] = 1/3
-            else :
-                proba[i][i+1] = 2/3
-        
-        else :
-            proba[i][i+1] = 1/3
-            proba[i][i+2] = 1/3
-            
-        #traps
-            
-    for i in range (nSquares-1):
-        for j in range(nSquares-1) :
-                
-            match layout[j] :
-                    
-                case 1 : #restart
-                    if j != 0 :
-                        proba[i][0] += proba[i][j]/2
-                        proba[i][j]/=2
-                            
-                case 2 : #penalty
-                    proba[i][j-3 if j-3>=0 else 0] += proba[i][j]/2
-                    proba[i][j]/=2
-                    
-                case 3 : #prison
-                    proba_prison[i][j] = proba[i][j]/2
-                    
-                case 4 : #mystery
-                        
-                    proba[i][j]/=2 #don't activate the trap
-                        
-                    if j != 0 :
-                        proba[i][0] += proba[i][j]/6 #trap restart
-                            
-                    proba[i][j-3 if j-3>=0 else 0] += proba[i][j]/6 #trap minus 3
-                        
-                    proba_prison[i][j] = proba[i][j]/6 #trap prison
-
-    proba[nSquares-1][nSquares-1] = 1
-
-    return proba, proba_prison
-
-def proba_risky_dice(layout, circle):
-    
-    proba = np.zeros((nSquares,nSquares))
-    proba_prison = np.zeros((nSquares,nSquares))
-    for i in range(nSquares-1):
-        
-        #1 chance out of 4 to stay at the same place
-        proba[i][i] = 1/4
-        
-        #1 chance out of 4 to go 1 further
-        #1 chance out of 4 to go 2 further
-        #1 chance out of 4 to go 3 further
-        
-        if i == 2 :
-            proba[i][i+1] = 1/8 #slow lane
-            proba[i][i+2] = 1/8 #slow lane
-            proba[i][i+3] = 1/8 #slow lane
-            
-            proba[i][i+8] = 1/8 #fast lane
-            proba[i][i+9] = 1/8 #fast lane
-            proba[i][i+10] = 1/8 #fast lane
-        
-        elif i == 7 :
-            proba[i][i+1] = 1/4
-            proba[i][i+2] = 1/4
-            proba[i][i+7] = 1/4
-            
-        elif i == 8 :
-            if circle :
-                proba[i][i+1] = 1/4
-                proba[i][i+6] = 1/4
-                proba[i][0] = 1/4
-            else :
-                proba[i][i+1] = 1/4
-                proba[i][i+6] = 1/2
-        
-        elif i == 9 :
-            if circle :
-                proba[i][i+5] = 1/4
-                proba[i][0] = 1/4
-                proba[i][1] = 1/4
-            else :
-                proba[i][i+5] = 3/4
-        
-                
-        elif i == 12 :
-            if circle :
-                proba[i][i+1] = 1/4
-                proba[i][(i+2)] = 1/4
-                proba[i][0] = 1/4
-            else :
-                proba[i][i+1] = 1/4
-                proba[i][i+2] = 1/2
-
-        elif i == 13 :
-            if circle :
-                proba[i][i+1] = 1/4
-                proba[i][0] = 1/4
-                proba[i][1] = 1/4
-            else :
-                proba[i][nSquares-1] = 3/4
-        
-        else :
-            proba[i][i+1] = 1/4
-            proba[i][i+2] = 1/4
-            proba[i][i+3] = 1/4
-         
-    #traps
-            
-    for i in range (nSquares-1):
-        for j in range(nSquares-1) :
-                
-            match layout[j] :
-                    
-                case 1 : #restart
-                    if j != 0 :
-                        proba[i][0] += proba[i][j]
-                        proba[i][j] = 0
-                            
-                case 2 : #penalty
-                    proba[i][j-3 if j-3>=0 else 0] += proba[i][j]
-                    proba[i][j] = 0
-                    
-                case 3 : #prison
-                    proba_prison[i][j] = proba[i][j]
-                    
-                case 4 : #mystery               
-                       
-                    if j != 0 :
-                        proba[i][0] += proba[i][j]/3 #trap restart
-                            
-                    proba[i][j-3 if j-3>=0 else 0] += proba[i][j]/3 #trap minus 3
-
-                    proba_prison[i][j] = proba[i][j]/3 #trap prison
-                    proba[i][j] /= 3
-
-    proba[nSquares-1][nSquares-1] = 1
-
-    return proba, proba_prison                        
-                
-    
 
 def markovDecision(layout,circle):
     """
@@ -250,11 +52,9 @@ def markovDecision(layout,circle):
     proba_risky, prison_risky = proba_risky_dice(layout,circle)
 
     value = np.zeros(nSquares)
-    newValue = np.arange(nSquares-1,-1,-1)
-    index=0
+    newValue = np.array([8.5,7.5,6.5,7,6,5,4,3,2,1,4,3,2,1,0])
+
     while (sum(abs(newValue-value)) > 1e-9):
-        index+=1
-        print(index)
         value = copy.deepcopy(newValue)
         for i in range (nSquares-1):
             newValue[i] = 1 + min(np.dot(proba_security[i],value) + np.dot(prison_security[i],value), np.dot(proba_normal[i],value) + np.dot(prison_normal[i],value), np.dot(proba_risky[i],value) + np.dot(prison_risky[i],value))
@@ -262,116 +62,17 @@ def markovDecision(layout,circle):
 
     dice = np.zeros(15, dtype=float)
     for i in range (nSquares):
-        dice[i] = np.argmin([np.dot(proba_security[i],value) + np.dot(prison_security[i],value), np.dot(proba_normal[i],value) + np.dot(prison_normal[i],value), np.dot(proba_risky[i],value) + np.dot(prison_risky[i],value)])
+        dice[i] = np.argmin(
+            [
+                np.dot(proba_security[i], newValue) + np.dot(prison_security[i], newValue),
+                np.dot(proba_normal[i], newValue) + np.dot(prison_normal[i], newValue),
+                np.dot(proba_risky[i], newValue) + np.dot(prison_risky[i], newValue),
+            ]
+        )
 
-    return newValue, dice
-
-
-    
-def playOneTurn(diceChoice, curPos, layout, circle, prison):
-    """
-    The function playOneTurn simulates one turn of the game, given the choice of dice (1 for “security” dice, 2 for “normal” dice and 3 for “risky”)
-    """
-    
-    if (curPos == nSquares-1) :
-        return nSquares-1, False
-    
-    if (prison):
-        return curPos, False
-
-    listDiceResults = [i for i in range(diceChoice+1)]
-    result = random.choice(listDiceResults)
-
-    if (curPos == 2 & result !=0) :
-        slowLane = random.choice([0,1])
-        if slowLane :
-            newPos = curPos + result
-        else :
-            newPos = curPos + result + 7
-
-    elif (curPos == 9 & result != 0):
-        newPos = curPos + result + 5
-
-    else : 
-        newPos = curPos + result
-    
-    if (newPos > nSquares-1) :
-        if (circle):
-            newPos -= nSquares
-        else :
-            return nSquares-1, True
-        
-        
-    newSquare = layout[newPos]
-
-    if(diceChoice == 1):
-        return newPos, False
-
-    if (diceChoice == 2):
-        newSquare = random.choice([0,newSquare])
-
-    match newSquare : 
-
-        case 0:
-            return newPos, False  # nothing happens
-
-        case 1 :
-            return 1, False #back to square one
-
-        case 2 : 
-            if (newPos - 3 < 1): 
-                return 1, False #back to square one
-            return newPos - 3, False # back 3 squares
-
-        case 3 : 
-            return newPos, True #prison
-
-        case 4 : 
-            newSquare = random.choice([1,2,3])
-
-            match newSquare : 
-
-                case 1:
-                    return 1, False  # back to square one
-
-                case 2 : 
-                    if (newPos - 3 < 1): 
-                        return 1, False #back to square one
-                    return newPos - 3, False # back 3 squares
-
-                case 3 : 
-                    return newPos, True #prison
+    return newValue, dice + 1
 
 
-def playOneGame(layout, circle):
-
-    # start of the game
-
-    curPos = 0 
-    prison = False 
-
-    if circle:
-
-        while (curPos != nSquares-1):
-            
-            newPos, prison = playOneTurn(diceChoice=3,curPos=curPos, layout=layout, circle=circle, prison=prison)
-            if (newPos > nSquares-1) :
-                curPos = nSquares - newPos
-            curPos = newPos
-            print(curPos)
-
-    if not circle : 
-
-        while curPos < nSquares-1:
-            
-            newPos, prison = playOneTurn(diceChoice=3, curPos=curPos, layout=layout, circle=circle, prison=prison)
-            curPos = newPos
-            print(curPos)
-                
-
-
-#playOneGame([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], True)
-
-val, dice = markovDecision([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], True)
+val, dice = markovDecision([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], False)
 print(val)
 print(dice)
